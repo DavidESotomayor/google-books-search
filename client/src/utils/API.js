2ec53552411d8302e9e0622bdacdab07a8521de7
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-04-13 01:59:27 -0500

    set up the api call methods in utils

---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export default {
+    // retrieves searched book from the Google Books API
+    googleBooks: function(search) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
+    },
+    // saves book to database
+    saveBook: function(bookData) {
+        return axios.post('/api/books', bookData)
+    },
+    // get saved book with a unique id
+    getBook: function(id) {
+        return axios.get('/api/books/' + id)
+    },
+    // get saved books from database
+    getBooks: function() {
+        return axios.get('/api/books')
+    },
+    // deletes a saved book with a unique id
+    deleteBook: function(id) {
+        return axios.delete('/api/books/' + id)
+    }
+}
